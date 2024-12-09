05aa224f5d1a6a1caeb9336b420c115a5c42d591
FGaribotZento <felipe.garibotti@zento.fr>
2024-12-09 01:04:35 +0100

    First Blog

---
+++ Astro/src/data/thinkings.js
@@ -0,0 +1,30 @@
+export const thinkings = [
+  {
+    title: 'First Thinking',
+    slug: 'first-thinking',
+    paragraphs: [
+      {
+        subtitle: 'Subtitle 1',
+        data: "This is the first paragraph of the first thinking.",
+      },
+      {
+        subtitle: 'Subtitle 2',
+        data: "This is another paragraph of the first thinking.",
+      },
+    ],
+    description: 'This is the first thought.',
+    layout: 'default', // Default layout
+  },
+  {
+    title: 'Second Thinking',
+    slug: 'second-thinking',
+    paragraphs: [
+      {
+        subtitle: 'Subtitle 1',
+        data: "This is the first paragraph of the second thinking.",
+      },
+    ],
+    description: 'This is the second thought.',
+    layout: 'special', // Special layout
+  },
+];
