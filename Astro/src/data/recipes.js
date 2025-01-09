4dfe958d28d09bd415ffdf9c0fe12db50c0dac1c
Ghilphar <felipegaribotti@gmail.com>
2025-01-10 00:13:21 +0100

    MODIF - Review of the changes on the layout of the recipes.

--- Astro/src/data/recipes.js
+++ Astro/src/data/recipes.js
@@ -1,18 +1,93 @@
 export const recipes = [
-    {
-      title: 'Chocolate Cake',
-      slug: 'chocolate-cake',
-      description: 'A rich and moist chocolate cake recipe.',
-    },
-    {
-      title: 'Pasta Carbonara',
-      slug: 'pasta-carbonara',
-      description: 'A creamy and delicious Italian pasta recipe.',
-    },
-    {
-      title: 'Grilled Cheese Sandwich',
-      slug: 'grilled-cheese',
-      description: 'The ultimate comfort food recipe with crispy bread and melted cheese.',
-    },
-  ];
-  
+  {
+    slug: 'chocolate-cake',
+    title: 'Chocolate Cake',
+    description: 'A delicious chocolate cake recipe.',
+    ingredients: [
+      '1 cup of flour',
+      '1/2 cup of sugar',
+      '1/4 cup of butter',
+      '1/4 cup of milk',
+    ],
+    steps: [
+      'Mix flour and sugar',
+      'Add butter and milk',
+      'Knead dough',
+      'Bake for 30 minutes',
+    ],
+    image: 'https://assets.tmecosys.com/image/upload/t_web600x528/img/recipe/vimdb/105823.jpg',
+  },
+  //Extend the data
+  {
+    slug: 'pumpkin-soup',
+    title: 'Pumpkin Soup',
+    description: 'A delicious pumpkin soup recipe.',
+    ingredients: [
+      '1 pumpkin',
+      '1 onion',
+      '1 cup of cream',
+      '1 cup of vegetable broth',
+    ],
+    steps: [
+      'Cut pumpkin and onion',
+      'Boil pumpkin and onion',
+      'Blend pumpkin and onion',
+      'Add cream and vegetable broth',
+    ],
+    image: 'https://assets.tmecosys.com/image/upload/t_web600x528/img/recipe/vimdb/105823.jpg',
+  },
+  {
+    slug: 'apple-pie',
+    title: 'Apple Pie',
+    description: 'A delicious apple pie recipe.',
+    ingredients: [
+      '2 cups of flour',
+      '1 cup of sugar',
+      '1/2 cup of butter',
+      '4 apples',
+    ],
+    steps: [
+      'Mix flour and sugar',
+      'Add butter and apples',
+      'Knead dough',
+      'Bake for 45 minutes',
+    ],
+    image: 'https://assets.tmecosys.com/image/upload/t_web600x528/img/recipe/vimdb/105823.jpg',
+  },
+  {
+    slug: 'pasta-salad',
+    title: 'Pasta Salad',
+    description: 'A delicious pasta salad recipe.',
+    ingredients: [
+      '2 cups of pasta',
+      '1 cup of cherry tomatoes',
+      '1/2 cup of olives',
+      '1/4 cup of feta cheese',
+    ],
+    steps: [
+      'Cook pasta',
+      'Chop tomatoes and olives',
+      'Mix all ingredients',
+      'Add feta cheese',
+    ],
+    image: 'https://assets.tmecosys.com/image/upload/t_web600x528/img/recipe/vimdb/105823.jpg',
+  },
+  {
+    slug: 'banana-bread',
+    title: 'Banana Bread',
+    description: 'A delicious banana bread recipe.',
+    ingredients: [
+      '2 cups of flour',
+      '1 cup of sugar',
+      '1/2 cup of butter',
+      '3 bananas',
+    ],
+    steps: [
+      'Mix flour and sugar',
+      'Add butter and bananas',
+      'Knead dough',
+      'Bake for 1 hour',
+    ],
+  },
+  // Add more recipes here
+];
